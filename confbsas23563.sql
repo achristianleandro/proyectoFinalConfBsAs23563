d67d111c5ec99f387be6ff156ac44a1316d2ac02
AlejandroGT18 <147356672+AlejandroGT18@users.noreply.github.com>
2023-12-11 15:11:13 -0300

    modifico la db confbsas23563.sql

--- confbsas23563.sql
+++ confbsas23563.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-12-2023 a las 00:51:06
+-- Tiempo de generación: 11-12-2023 a las 13:39:02
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,21 +36,22 @@ CREATE TABLE `oradores` (
   `fotoPerfil` varchar(300) DEFAULT NULL,
   `dni` int(11) NOT NULL,
   `tel` int(11) DEFAULT NULL,
-  `email` varchar(70) DEFAULT NULL
+  `email` varchar(70) DEFAULT NULL,
+  `etiquetasDos` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `oradores`
 --
 
-INSERT INTO `oradores` (`id`, `nombre`, `apellido`, `etiquetas`, `temas`, `fotoPerfil`, `dni`, `tel`, `email`) VALUES
-(3, 'Steve', 'Jobs', 'Transcof', 'Inicios de Apple, tecnologías utilizadas en las Mac. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/80eee944aad1ca133afd5f64e95069ef411ea60d845497dbc9f5bf2916524ad8.png', 35468465, NULL, 'sjobs@gmail.com'),
-(4, 'Bill', 'Gates', 'Hatity', 'Nacimiento de Windows,  historia.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/1d81e66905ab291ce139feedfdedbcbf06dfabf61aa0ad0b5ae96c45cc6f06a4.png', 23556486, NULL, 'bgates@microsoft.com'),
-(15, 'Ada', 'Lovelace', '', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/f443e2b224b2574ee557f5fc5663e8bc0957c5db3d6b0a8ee85b08630557737b.jpeg', 35466846, NULL, 'alovelace@gmail.com'),
-(16, 'Steve', 'Wozniak', '', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/f9cf44c1a8877fbf6d5f2e8d70ba38b32549e52da122d8680f2968e34f8acdc1.jpg', 36589788, NULL, 'swozniak@gmail.com'),
-(28, 'Elon', 'Musk', '', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/0b1302c12d8ed355c5b8891ccc7c484f48f5a2e18f96e1b55b7ab5fc0d8f6e9f.jpg', 45655489, NULL, 'emusk@gmail.com'),
-(31, 'Michelle', 'Obama', '', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/cfa0481eabb062e57d8feed2206accbee04a77dec57feb0aa6cfe92699968138.jpg', 32298753, NULL, 'mobama@gmail.com'),
-(33, 'Greta', 'Thunberg', '', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/82ff37a80c9c8b434822fa86bae6fdc2d4e9a05d831990ecdf48e8dee2efab4c.jpg', 65458975, NULL, 'gthunberg@gmail.com');
+INSERT INTO `oradores` (`id`, `nombre`, `apellido`, `etiquetas`, `temas`, `fotoPerfil`, `dni`, `tel`, `email`, `etiquetasDos`) VALUES
+(3, 'Steve', 'Jobs', 'Java Script', 'Inicios de Apple, tecnologías utilizadas en las Mac. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/80eee944aad1ca133afd5f64e95069ef411ea60d845497dbc9f5bf2916524ad8.png', 35468465, NULL, 'sjobs@gmail.com', 'React'),
+(4, 'Bill', 'Gates', 'JAVA', 'Nacimiento de Windows,  historia.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/1d81e66905ab291ce139feedfdedbcbf06dfabf61aa0ad0b5ae96c45cc6f06a4.png', 23556486, NULL, 'bgates@microsoft.com', 'C++'),
+(15, 'Ada', 'Lovelace', 'Negocios', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/f443e2b224b2574ee557f5fc5663e8bc0957c5db3d6b0a8ee85b08630557737b.jpeg', 35466846, NULL, 'alovelace@gmail.com', 'Startups'),
+(16, 'Steve', 'Wozniak', 'Phyton', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/f9cf44c1a8877fbf6d5f2e8d70ba38b32549e52da122d8680f2968e34f8acdc1.jpg', 36589788, NULL, 'swozniak@gmail.com', NULL),
+(28, 'Elon', 'Musk', 'C#', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/0b1302c12d8ed355c5b8891ccc7c484f48f5a2e18f96e1b55b7ab5fc0d8f6e9f.jpg', 45655489, NULL, 'emusk@gmail.com', NULL),
+(31, 'Michelle', 'Obama', 'Java Script', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/cfa0481eabb062e57d8feed2206accbee04a77dec57feb0aa6cfe92699968138.jpg', 32298753, NULL, 'mobama@gmail.com', 'Python'),
+(33, 'Greta', 'Thunberg', 'Startups', '. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/82ff37a80c9c8b434822fa86bae6fdc2d4e9a05d831990ecdf48e8dee2efab4c.jpg', 65458975, NULL, 'gthunberg@gmail.com', NULL);
 
 -- --------------------------------------------------------
 
