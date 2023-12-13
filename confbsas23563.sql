5bf68b4829e538e344f6e5ee85c5fbfee7395870
ChristianAcuña <achristianleandro@gmail.com>
2023-12-13 20:39:56 -0300

    actualizo bbdd

--- confbsas23563.sql
+++ confbsas23563.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-12-2023 a las 00:12:20
+-- Tiempo de generación: 14-12-2023 a las 00:39:03
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -46,13 +46,13 @@ CREATE TABLE `oradores` (
 
 INSERT INTO `oradores` (`id`, `nombre`, `apellido`, `etiquetas`, `temas`, `fotoPerfil`, `dni`, `tel`, `email`, `etiquetasDos`) VALUES
 (1, 'Steve', 'Jobs', 'Java Script', 'Inicios de Apple, tecnologías utilizadas en las Mac. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/80eee944aad1ca133afd5f64e95069ef411ea60d845497dbc9f5bf2916524ad8.png', 35468465, NULL, 'sjobs@gmail.com', 'React'),
-(2, 'Bill', 'Gates', 'JAVA', 'Nacimiento de Windows,  historia.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/1d81e66905ab291ce139feedfdedbcbf06dfabf61aa0ad0b5ae96c45cc6f06a4.png', 23556486, NULL, 'bgates@microsoft.com', 'C++'),
+(2, 'Bill', 'Gates', 'JAVA', 'Nacimiento de Windows,  historia.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/1d81e66905ab291ce139feedfdedbcbf06dfabf61aa0ad0b5ae96c45cc6f06a4.png', 23556486, NULL, 'bgates@gmail.com', 'C++'),
 (3, 'Ada', 'Lovelace', 'Negocios', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/f443e2b224b2574ee557f5fc5663e8bc0957c5db3d6b0a8ee85b08630557737b.jpeg', 35466846, NULL, 'alovelace@gmail.com', 'Startups'),
 (4, 'Steve', 'Wozniak', 'Apple', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/f9cf44c1a8877fbf6d5f2e8d70ba38b32549e52da122d8680f2968e34f8acdc1.jpg', 36589788, NULL, 'swozniak@gmail.com', 'Tecnologia'),
 (5, 'Elon', 'Musk', 'C#', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/0b1302c12d8ed355c5b8891ccc7c484f48f5a2e18f96e1b55b7ab5fc0d8f6e9f.jpg', 45655489, NULL, 'emusk@gmail.com', 'Java'),
 (6, 'Michelle', 'Obama', 'Java Script', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/cfa0481eabb062e57d8feed2206accbee04a77dec57feb0aa6cfe92699968138.jpg', 32298753, NULL, 'mobama@gmail.com', 'Python'),
-(7, 'Greta', 'Thunberg', 'Medioambiente', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgdb.net/storage/uploads/82ff37a80c9c8b434822fa86bae6fdc2d4e9a05d831990ecdf48e8dee2efab4c.jpg', 65458975, NULL, 'gthunberg@gmail.com', 'Naturaleza'),
-(8, 'Lionel', 'Messi', 'Futbol', 'Historias de Messi en el Barcelona', 'https://imgdb.net/storage/uploads/08c14952b03b3364b59ab68fc9b665ee7e8df814c42e2eefb6a0d1d649cf69e1.jpg', 65489757, NULL, 'lmessi@gmail.com', 'Barcelona');
+(7, 'Greta', 'Thunberg', 'Naturaleza', 'Naturaleza y medioambiente', 'https://imgdb.net/storage/uploads/82ff37a80c9c8b434822fa86bae6fdc2d4e9a05d831990ecdf48e8dee2efab4c.jpg', 65458975, NULL, 'gthunberg@gmail.com', 'Medioambiente'),
+(8, 'Lionel', 'Messi', 'Barcelona', 'Equipos de leo', 'https://imgdb.net/storage/uploads/08c14952b03b3364b59ab68fc9b665ee7e8df814c42e2eefb6a0d1d649cf69e1.jpg', 65489757, NULL, 'lmessi@gmail.com', 'Paris');
 
 -- --------------------------------------------------------
 
